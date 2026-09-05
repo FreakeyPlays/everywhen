de8f8bcc0e28d4e9686470a938c875409333aea0
Christoph Merck <78086475+FreakeyPlays@users.noreply.github.com>
2026-09-05 21:35:26 +0200

    inital setup without tests

---
+++ apps/client/projects/ui/.storybook/main.ts
@@ -0,0 +1,9 @@
+import type { StorybookConfig } from "@storybook/angular-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.ts"],
+  addons: ["@storybook/addon-a11y", "@storybook/addon-docs"],
+  framework: "@storybook/angular-vite",
+};
+
+export default config;
---
+++ apps/client/projects/ui/.storybook/preview.ts
@@ -0,0 +1,19 @@
+import type { Preview } from "@storybook/angular-vite";
+
+import "./styles.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    a11y: {
+      test: "todo",
+    },
+  },
+};
+
+export default preview;
